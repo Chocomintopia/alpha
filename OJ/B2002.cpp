3d92180b6be44579599d5e6c5b95f731a255e3e4
Y Ec <55490538+DesafinadocUNu@users.noreply.github.com>
2024-11-19 11:06:52 +0800

    Create B2002.cpp

---
+++ OJ/B2002.cpp
@@ -0,0 +1,20 @@
+// 2024/11/19
+// Hello,World!
+//
+// 编写一个能够输出 `Hello,World!` 的程序。
+//
+// 提示：
+// - 使用英文标点符号；
+// - `Hello,World!` 逗号后面**没有**空格。
+// - `H` 和 `W` 为**大写**字母。
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello,World!" << endl; // endl表示换行
+    return 0;
+}
+
+// g++ B2002.cpp
+// ./a.exe
